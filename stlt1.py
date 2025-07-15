d5454ac8a3d92488f6b363026ccd2c73b3b4c8b9
indrani-gthb1 <indranijiko@gmail.com>
2025-07-15 22:51:19 +0530

    Add files via upload

--- stlt1.py
+++ stlt1.py
@@ -1,7 +1,4 @@
 import streamlit as st
 st.title("hello i am workinng in streamlit")
+name-st.text_input("Enter your name")
 st.write("hello...")
-name=st.button("submit")
-if st.button("submit"):
-  st.write("hi,{name} welcome to streamlit")
-  
